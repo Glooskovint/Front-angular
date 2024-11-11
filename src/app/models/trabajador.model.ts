e34d289378022f7eac05201b86922185558580cf
Glooskovint <angelogonzalesteran@gmail.com>
2024-11-11 10:48:09 -0500

    Se crearon los modelos del proyecto

---
+++ src/app/models/trabajador.model.ts
@@ -0,0 +1,6 @@
+export interface Trabajador {
+    id?: number;           // opcional en caso de creación
+    nombre: string;
+    rol: string;
+  }
+  
---
+++ src/app/models/venta.model.ts
@@ -0,0 +1,11 @@
+import { Trabajador } from './trabajador.model';
+import { Producto } from './producto.model';
+import { Cliente } from './cliente.model';
+
+export interface Venta {
+  id?: number;
+  trabajador: Trabajador;
+  producto: Producto;
+  cliente: Cliente;
+  fecha: Date;
+}
