a16474173bea837944aaba3860f6efd8ffff8e54
Glooskovint <angelogonzalesteran@gmail.com>
2024-11-11 11:06:26 -0500

    Primera prueba de componentes, listar trabajadores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { TrabajadorListComponent } from './components/trabajador/trabajador-list/trabajador-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'trabajadores', component: TrabajadorListComponent },
+    //{ path: 'trabajadores/nuevo', component: TrabajadorFormComponent },
+    //{ path: 'trabajadores/editar/:id', component: TrabajadorFormComponent },
+    { path: '', redirectTo: '/trabajadores', pathMatch: 'full' }  // Redirección inicial
+];
+
+export const appRouter = provideRouter(routes);
